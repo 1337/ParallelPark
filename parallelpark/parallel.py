8094e4d235f9d9d933d8c27a8fd80f5fc2ba3f32
Brian Lai <b2lai@uwaterloo.ca>
2014-03-21 14:59:50 -0400

    Simplify interface

--- parallelpark/parallel.py
+++ parallelpark/parallel.py
@@ -1,100 +1,89 @@
+#!/usr/bin/env python
+
+"""
+More elaborate version of "Parallelism in one line"
+https://medium.com/building-things-on-the-internet/40e9b2b36148
+"""
+
 
 __copyright__ = 'Willet Inc.'
 __author__    = 'Brian Lai'
 
-try:
-    from concurrent import futures
-except ImportError as err:
-    import futures
-    
+
+from multiprocessing.dummy import Pool as ThreadPool
 from functools import partial
 
 
 class ParallelPark(object):
 
-    fns = []
+    data = None
     args = tuple()
     kwargs = {}
-    workers = 4
+    workers = None
 
-    def __init__(self, *fns, **kwargs):
+    async_result = None
+
+    def __init__(self, data, args=None, kwargs=None, worker_count=4):
         """
-        :param fns tuple
-        kwargs allowed: args, kwargs, workers
+        :param {list<{function}>} data    an iterable for multiprocessing
+        :param {list|tuple}       args    a list of positional arguments to
+                                          pass to the functions
+        :param {dict}             kwargs  KVs of named arguments to pass
+                                          to the functions
+        :param {int}              workers number of threads to use
         """
-        if type(fns[0]) == list:  # passed in as [fn, fn, fn]; unpack
-            self.fns = fns[0]
-        else:
-            self.fns = fns
+        if not args:
+            args = tuple()
+        if not kwargs:
+            kwargs = {}
 
-        self._update(kwargs)
+        self.data, self.workers, self.args, self.kwargs = (
+            data, ThreadPool(worker_count), args, kwargs)
 
-    def _update(self, dct):
-        for key in dct:
-            setattr(self, key, dct[key])
+    def __iter__(self):
+        for result in self.values:
+            yield result
 
-    def map(self, data=None, as_generator=True):
+    def map(self, fn, *args, **kwargs):
         """
-        fn2(fn1(data, *args, **kwargs), *args, **kwargs)
-
-        :param as_generator (no effect in Python 3+)
-
-        :returns generator
+        :returns self
         """
+        if not fn:
+            return self  # done
 
-        if not data:
-            data = []
+        if self.async_result:
+            self.async_result.wait()
+            self.data = self.async_result.get()
 
-        intermediates = data
-        for fn in self.fns:
-            with futures.ThreadPoolExecutor(max_workers=self.workers) as executor:
-                fish_curry = partial(fn, *self.args, **self.kwargs)
-                intermediates = executor.map(fish_curry, intermediates)
+        args += self.args
+        kwargs.update(self.kwargs)
 
-        if as_generator and hasattr(intermediates, '__iter__'):
-            return intermediates
-        return list(intermediates)
+        fish_curry = partial(fn, *args, **kwargs)
+        intermediates = self.workers.map_async(fish_curry, self.data)
 
-    def run(self, data=None, as_generator=True):
-        """class(def 1, def 2).execute()
+        self.async_result = intermediates
 
-        :param as_generator (no effect in Python 3+)
+        return self
 
-        :returns list
+    @property
+    def values(self):
+        """Get the final values from all the maps
+
+        :returns generator (there's no reason to return a list)
         """
-        len_fns = len(self.fns)
-        idx_map = dict(zip(range(len_fns), self.fns))
+        if self.workers:
+            self.workers.close()
+            self.workers.join()
+            self.data = self.async_result.get()
 
-        with futures.ThreadPoolExecutor(max_workers=self.workers) as executor:
-            future_to_idx = dict([
-                (executor.submit(idx_map[idx], data,
-                                 *self.args, **self.kwargs), idx)
-                for idx in idx_map
-            ])
-
-        for future in futures.as_completed(future_to_idx):
-            idx = future_to_idx[future]
-            idx_map[idx] = future.result()
-
-        if as_generator and hasattr(idx_map, 'itervalues'):
-            return idx_map.itervalues()
-
-        return idx_map.values()
-
-    def then(self, fn):
-        pass
-
-    def fail(self, handler):
-        pass
+        return self.data
 
 
 if __name__ == '__main__':
     # Test map
-    a = ParallelPark(lambda x: x + 5).map([1, 2, 3])
-    for x in a:
-        print(x)
+    def pow(number, exponent):
+        return number ** exponent
 
-    # Test run
-    a = ParallelPark(lambda x: x + 5, lambda x: x - 5).run(1)
+    a = ParallelPark(xrange(10)).map(pow, exponent=5).map(lambda x: x + 2)
     for x in a:
         print(x)
